f15c132b3d515a288c9ba878d36daf11429a39a6
haein <yuri_0_@hanmail.net>
2024-08-08 18:26:11 +0900

    회원가입,로그인 이미지 일그러짐 수정

--- member/js/login.js
+++ member/js/login.js
@@ -1,4 +1,4 @@
-const _ipUser_pw = document.querySelector('#user_pw');    // password input 박스
+const _ipUserPass = document.querySelector('#user_pass');    // password input 박스
 const _imgPassEye = document.querySelector('#pass_eye');  // password input 박스 내 눈 표시 아이콘
 
 // 패스워드 눈 아이콘 클릭시 이벤트 발생
@@ -8,12 +8,12 @@ _imgPassEye.addEventListener('click', () => {
     // 비밀번호 숨김 > 보여줌으로 변경
     _imgPassEye.classList.replace('pass_hide', 'pass_show');
     _imgPassEye.src = "../images/icons/free-icon-eye.png";
-    _ipUser_pw.type = 'text';
+    _ipUserPass.type = 'text';
   }else if(_imgPassEye.classList.contains('pass_show')) {
     // 비밀번호 보여줌 > 숨김으로 변경
     _imgPassEye.classList.replace('pass_show', 'pass_hide');
     _imgPassEye.src = "../images/icons/free-icon-eye-hidden.png";
-    _ipUser_pw.type = 'password';
+    _ipUserPass.type = 'password';
   }
 });
 
